9487ee42e2c5a7fa6301867973e3aa43c461cfd4
h0ch1 <h0ch1@naver.com>
2021-08-15 14:33:54 +0000

    .

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const dburl = "mongodb+srv://h0ch1:a02070203@nodetest.kijps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.set('view engine' , 'ejs');
+app.use('/public', express.static('public'));
+
+var db
+MongoClient.connect(dburl, (err, client) => {
+    if(err) return console.log(err);
+
+    db = client.db('armydate');
+
+    app.listen(8080, () => {
+        console.log('server start');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+

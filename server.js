7e069519f81e24219fc1e609d4d4a55f9af660ee
h0ch1 <h0ch1@naver.com>
2021-08-17 23:44:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
